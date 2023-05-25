8e23cf1df5bccdea4d4acb0ab2c4c1b055533812
wjs <swilfredojoel@gmail.com>
2023-05-25 11:14:56 -0300

    vs.1.0 - all functions work

--- libs/runLibs.bat
+++ libs/runLibs.bat
@@ -1,11 +1,9 @@
 gcc -c matrix.c -o matrix.o
-ar rsc libMatrix.a matrix.o
 
 gcc -c fileReader.c -o fileReader.o
-ar rsc libFileReader.a fileReader.o
 
 gcc -c operations.c -o operations.o -L. -l:libMatrix.a
-ar rsc libOperations.a operations.o 
 
-gcc -c dataCollector.c -o dataCollector.o -L. -l:libOperations.a -l:libMatrix.a 
-ar rsc libDataCollector.a dataCollector.o
+gcc -c dataCollector.c -o dataCollector.o -L. -l:libOperations.a -l:libMatrix.a
+
+ar rsc lib.a matrix.o fileReader.o operations.o dataCollector.o
--- main/main.c
+++ main/main.c
@@ -2,26 +2,10 @@
 #include "../libs/operations.h"
 #include "../libs/dataCollector.h"
 #include "../libs/fileReader.h"
+#include <stdio.h>
 
 int main()
 {
-  Matrix m1;
-  initMatrixWithId(&m1, 3, 3, 'a');
-
-  for (int i=0; i < 3; i++)
-  {
-    for (int j=0; j < 3; j++)
-    {
-      m1.data[i][j] = i+1 * j+2;
-    }
-  }
-
-  showMatrix(m1);
-
-  Matrix temp;
-  temp = format('t', &m1);
-
-  showMatrix(temp);
 
   return 0;
 }
